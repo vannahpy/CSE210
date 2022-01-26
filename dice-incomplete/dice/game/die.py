0c408f625dc8d5fbcb69ee726312b03d107aa4cb
Vannah Pyatt <pya17001@byui.edu>
2022-01-25 18:40:01 -0700

    again

--- dice-incomplete/dice/game/die.py
+++ dice-incomplete/dice/game/die.py
@@ -1,35 +1,24 @@
 import random
 
-
-# TODO: Implement the Die class as follows...
-
-# 1) Add the class declaration. Use the following class comment.
-    """A small cube with a different number of spots on each of its six sides.
-
-    The responsibility of Die is to keep track of the side facing up and calculate the points for 
-    it.
-   
-    Attributes:
-        value (int): The number of spots on the side facing up.
-        points (int): The number of points the die is worth.
-    """
-
-# 2) Create the class constructor. Use the following method comment.
-        """Constructs a new instance of Die with a value and points attribute.
-
-        Args:
-            self (Die): An instance of Die.
-        """
-
-# 3) Create the roll(self) method. Use the following method comment.
-        """Generates a new random value and calculates the points.
-        
-        Args:
-            self (Die): An instance of Die.
-        """
-
 class Die:
+    """A small cube with a different number of spots on each of its six sides. \
+    The responsibility of Die is to keep track of the side facing up and calculate the points for   it.
+    Attributes:
+    value (int): The number of spots on the side facing up.
+    points (int): The number of points the die is worth."""
 
     def _init_(self):
-        pass
-    pass
+        """Constructs a new instance of Die with a value and points attribute
+        Args: self (Die): An instance of Die."""
+        self.value = random.randint(1, 6, endpoint = True)
+        if self.value == 1:
+            self.points = 100
+        elif self.value == 5:
+            self.points = 50 
+        else:
+            self.points = 0
+
+#    def roll(self):
+#        """Generates a new random value and calculates the points.
+#        Args: self (Die): An instance of Die."""
+#        self.score = 
